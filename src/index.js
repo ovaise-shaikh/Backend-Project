4a9a8197f92914384ef0566d21e60c8e6bda4e44
ovaise-shaikh <ovaise19@gmail.com>
2025-04-14 17:40:09 +0530

    add api responses and error handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on Port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed!! ", error);
+})
 
 
 
