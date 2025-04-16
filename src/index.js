21f54fd24df92c1176d124bd62a532ac6cb622ca
ovaise-shaikh <ovaise19@gmail.com>
2025-04-16 12:54:28 +0530

    check and configure register end points

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
-
-const app = express();
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 connectDB()
@@ -23,7 +21,6 @@ connectDB()
 .catch((error) => {
     console.log("MongoDB connection failed!! ", error);
 })
-
 
 
 
